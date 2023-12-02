b4a12793358a6c011f32bdda28f98a6b4d4567b7
Dev Gaurav Jatt <gaurav4x@onmail.com>
2023-12-02 14:25:55 +0530

    best ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,18 +74,21 @@
 					on:click={() => getPost(0)}
 					class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 				>
-					Prev
 					<svg
-						class="-mr-1 ml-2 h-4 w-4"
-						fill="currentColor"
-						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-							clip-rule="evenodd"
-						></path></svg
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="mr-2 h-4 w-4"
 					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+						/>
+					</svg>
+					Prev
 				</button>
 				<button
 					on:click={() => getPost(1)}
@@ -93,16 +96,19 @@
 				>
 					Next
 					<svg
-						class="-mr-1 ml-2 h-4 w-4"
-						fill="currentColor"
-						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-							clip-rule="evenodd"
-						></path></svg
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="ml-2 h-4 w-4"
 					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+						/>
+					</svg>
 				</button>
 			</div>
 		</div>
